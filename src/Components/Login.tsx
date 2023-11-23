410627787c114771cd372906126c82c7f29cd5a4
akeemkabiru <kabby.dev@outlook.com>
2023-11-23 16:55:53 +0000

    addform

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -6,7 +6,7 @@ export default function Login() {
       <form className="bg-white flex flex-col gap-5 w-[300px] items-center rounded-lg py-5">
         <h2 className="text-3xl font-bold text-center mb-5">Login</h2>
         <div>
-          <label htmlFor="username" className="text-xl font-semibold">
+          <label htmlFor="username" className="text-lg font-semibold">
             Username
           </label>
           <div>
@@ -15,12 +15,12 @@ export default function Login() {
               name=""
               id=""
               placeholder="Enter your username"
-              className="border-b-4 bg-transparent rounded placeholder:text-gray-300 placeholder:font-semibold placeholder:text-lg w-[250px] p-2 mt-3 placeholder:text-center"
+              className="border-b-4 bg-transparent rounded placeholder:text-gray-300 placeholder:font-semibold placeholder:text-lg w-[250px] p-2 mt-2 placeholder:text-center focus:border-blue-900 outline-none transition-colors duration-1000"
             />
           </div>
         </div>
         <div>
-          <label htmlFor="password" className="text-xl font-semibold">
+          <label htmlFor="password" className="text-lg font-semibold">
             Password
           </label>
           <div>
@@ -30,7 +30,7 @@ export default function Login() {
               name=""
               id=""
               placeholder="Enter your password"
-              className="border-b-4 bg-transparent rounded placeholder:text-gray-300 placeholder:font-semibold placeholder:text-lg w-[250px] p-2 mt-3 placeholder:text-center"
+              className="border-b-4 bg-transparent rounded placeholder:text-gray-300 placeholder:font-semibold placeholder:text-lg w-[250px] p-2 mt-2 placeholder:text-center focus:border-blue-900 transition-colors duration-1000 outline-none"
             />
           </div>
         </div>
@@ -40,7 +40,7 @@ export default function Login() {
         >
           Log in
         </button>
-        <div className="mt-10">
+        <div className="mt-20">
           <h3 className="text-xl font-semibold mb-3">Or sign in with</h3>
           <div className="flex items-center justify-center gap-2">
             {" "}
