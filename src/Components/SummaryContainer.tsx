0c0627800cbe611e1619c55f664a436461029f2f
akeemkabiru <kabby.dev@outlook.com>
2023-11-27 12:51:55 +0000

    responsive

--- src/Components/SummaryContainer.tsx
+++ src/Components/SummaryContainer.tsx
@@ -8,7 +8,7 @@ export default function SummaryContainer({
   return (
     <div className="h-[100px] sm:w-[200px] w-[300px] border-2 rounded-xl border-gray-300 flex pl-5  flex-col justify-evenly  ">
       <div>{detail}</div>
-      <div className="text-3xl font-semibold">{value}</div>
+      <div className="sm:text-3xl text-2xl font-semibold">{value}</div>
     </div>
   );
 }
