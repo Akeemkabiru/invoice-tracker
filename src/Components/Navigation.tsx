af4978cd34f2257db7da89504461672fd04c2d85
akeemkabiru <kabby.dev@outlook.com>
2023-11-28 08:52:05 +0000

    responsive

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -9,10 +9,11 @@ export default function Navigation() {
       <div className="flex items-center gap-5 text-lg">
         <h1 className=" font-sans-serif text-4xl font-semibold">Fl</h1>{" "}
         <div
-          className={`sm:flex flex flex-col sm:flex-row fixed top-0 sm:relative font-semibold left-0 w-[250px] sm:w-auto h-full sm:h-auto sm:gap-5 text-white sm:text-black bg-black px-5 pt-20 sm:p-0 gap-10 sm:bg-white ${
+          className={`sm:flex flex flex-col sm:flex-row fixed top-0 sm:relative font-semibold left-0 w-[250px] sm:w-auto h-full sm:h-auto sm:gap-5 text-white sm:text-black bg-black px-5 pt-36 sm:p-0 gap-10 sm:bg-white ${
             open ? "block" : "hidden"
           } `}
         >
+          <h1 className="absolute top-5 left-5 text-4xl font-semibold">Fl</h1>
           <X
             className="absolute top-5 right-5"
             size={`${30}px`}
