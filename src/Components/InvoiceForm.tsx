002f0b41d1ce81ee34ede08a608493aaddf8c5c0
akeemkabiru <kabby.dev@outlook.com>
2023-12-01 18:29:39 +0000

    commit

--- src/Components/InvoiceForm.tsx
+++ src/Components/InvoiceForm.tsx
@@ -4,7 +4,7 @@ type InvoiceFormProp = {
 };
 export default function InvoiceForm({ OnClick }: InvoiceFormProp) {
   return (
-    <div className="fixed sm:w-[50vw] sm:h-[50vh] w-[80vw] h-[60vh] rounded-md sm:top-[25%] sm:left-[25%] bg-white sm:px-20 z-10 top-[20vh] left-[10vw] px-5">
+    <div className="fixed sm:w-[50vw] sm:h-[50vh] w-[80vw] h-[60vh] rounded-md sm:top-[20%] sm:left-[25%] bg-white sm:px-20 z-10 top-[15vh] left-[10vw] px-5">
       <X className="absolute right-5 top-5" onClick={OnClick} />
       <div className="flex flex-col gap-5 sm:mt-[10%] mt-[12vh]">
         <input
